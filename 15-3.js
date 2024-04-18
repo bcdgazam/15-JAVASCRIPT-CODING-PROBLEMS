a6655b33b9fbc83abe00154742d9659be3c1eb21
Golam Azam <bcdazam@gmail.com>
2024-04-19 00:39:10 +0600

    floor, ceil, round, random

---
+++ 15-3.js
@@ -0,0 +1,18 @@
+//15-3 Random number, random number between 1 to 6
+
+let num = 2.5;
+let result = Math.floor(num);
+console.log(result);
+
+let result2 = Math.ceil(num);
+console.log(result2);
+
+let result3 = Math.round(num);
+console.log(result3);
+
+let dice = Math.random(num);
+console.log(dice);
+
+let dice2 = Math.random(num) * 10;
+let randomIntNumber = Math.round(dice2);
+console.log(randomIntNumber);

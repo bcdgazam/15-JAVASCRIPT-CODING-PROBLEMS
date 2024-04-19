c871b0925d5930639008ee0ba0aa9efb3812c2de
Golam Azam <bcdazam@gmail.com>
2024-04-19 15:16:09 +0600

    largest number from an array

---
+++ 15-5.js
@@ -0,0 +1,14 @@
+//15-5 Find the largest element of an array
+
+let stResul = [23, 50, 60, 10, 20, 98, 100, 102, 97, 96, 80, 103, 151, 50]
+
+let max = stResul[0];
+
+for(let i = 0; i < stResul.length; i++){
+    let element = stResul[i];
+    if(element > max){
+        max = element;
+    }
+}
+
+console.log('The heighst atel is:', max);

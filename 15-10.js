d37237d261a3991604e329df973d33bcd4f62e8e
Golam Azam <bcdazam@gmail.com>
2024-04-21 10:09:52 +0600

    exercise

---
+++ 15-10.js
@@ -0,0 +1,17 @@
+//15-10 Module summary
+
+/*
+    15-1 Module Introduction, problem setup
+    15-2 Swap variable, swap without temp, destructing
+    15-3 Random number, random number between 1 to 6
+    15-4 Find max of two values, find max of three values
+    15-5 Find the largest element of an array
+    15-6 Sum of all numbers in an array
+    15-7 Remove duplicate item from an array
+    15-8 Count the number of words in a string
+    15-9 Reverse a string
+    15-10 Module summary
+*/
+
+
+//Exercise

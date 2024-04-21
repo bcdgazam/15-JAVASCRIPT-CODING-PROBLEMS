89351b74e2b41173fb2cb91e3e3e3927a9362293
Golam Azam <bcdazam@gmail.com>
2024-04-21 10:37:19 +0600

    exercise

--- 15-10.js
+++ 15-10.js
@@ -1,17 +1,24 @@
 //15-10 Module summary
 
 /*
-    15-1 Module Introduction, problem setup
-    15-2 Swap variable, swap without temp, destructing
-    15-3 Random number, random number between 1 to 6
-    15-4 Find max of two values, find max of three values
-    15-5 Find the largest element of an array
-    15-6 Sum of all numbers in an array
-    15-7 Remove duplicate item from an array
-    15-8 Count the number of words in a string
-    15-9 Reverse a string
-    15-10 Module summary
+    1 Swap variable, swap without temp, destructing
+    2 Random number, random number between 1 to 6
+    3 Find max of two values, find max of three values
+    4 Find the largest element of an array
+    5 Sum of all numbers in an array
+    6 Remove duplicate item from an array
+    7 Count the number of words in a string
+    8 Reverse a string
 */
 
 
 //Exercise
+
+let a = 5;
+let b = 10;
+console.log('Before swap:',a, b)
+
+let temp = a;
+a = b;
+b = temp;
+console.log('After swap:', a, b)

216f77d6690c3b455ca9d48342aee18a2224b139
Golam Azam <bcdazam@gmail.com>
2024-04-28 15:34:36 +0600

    exercise

--- pr.js
+++ pr.js
@@ -1,12 +1,10 @@
 //Practice folder
 
-/*
-    1 Swap variable, swap without temp, destructing
-    2 Random number, random number between 1 to 6
-    3 Find max of two values, find max of three values
-    4 Find the largest element of an array
-    5 Sum of all numbers in an array
-    6 Remove duplicate item from an array
-    7 Count the number of words in a string
-    8 Reverse a string
-*/
+let a = 10;
+let b = 20;
+console.log('before swap a =', a , 'b =', b)
+
+let temp = a;
+a = b;
+b = temp;
+console.log('After swap a =',a, 'b=', b);

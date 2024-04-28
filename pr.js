d8f3f02e8430e73b4651689decabbd06090501e9
Golam Azam <bcdazam@gmail.com>
2024-04-28 15:04:24 +0600

    practice

---
+++ pr.js
@@ -0,0 +1,12 @@
+//Practice folder
+
+/*
+    1 Swap variable, swap without temp, destructing
+    2 Random number, random number between 1 to 6
+    3 Find max of two values, find max of three values
+    4 Find the largest element of an array
+    5 Sum of all numbers in an array
+    6 Remove duplicate item from an array
+    7 Count the number of words in a string
+    8 Reverse a string
+*/

5a205a35f3faffa63e3b523c802274e7764594c7
Golam Azam <bcdazam@gmail.com>
2024-05-01 23:59:55 +0600

    practice

--- pr.js
+++ pr.js
@@ -1,8 +1,8 @@
 //Practice file
 
 
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
 console.log('Before swap a =', a, 'b =', b)
 
@@ -12,7 +12,19 @@ console.log('Before swap a =', a, 'b =', b)
 
 // console.log('After swap a = ', a, 'b = ', b)
 
-let result = a +b;
-a = result - a;
-b = result - b;
-console.log('After swap a = ', a, 'b = ', b)
+// a = a + b;
+// a = a - b;
+// b = b - a;
+// console.log('After swap a = ', a, 'b = ', b)
+
+//another way
+// let a = 10;
+// let b = 20;
+// [a, b] = [b, a];
+// console.log('After swap a = ', a, 'b = ', b);
+
+let p = 5;
+let q = 7;
+
+[p, q] = [q, p];
+console.log("after swap: p =", p,",", "q =", q)

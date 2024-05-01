54936210af602b53d56d17c66b3e13f3232b8585
Golam Azam <bcdazam@gmail.com>
2024-05-01 23:51:19 +0600

    without temp

--- pr.js
+++ pr.js
@@ -6,8 +6,13 @@ let b = 20;
 
 console.log('Before swap a =', a, 'b =', b)
 
-temp = a;
-a = b;
-b = temp;
+// temp = a;
+// a = b;
+// b = temp;
 
+// console.log('After swap a = ', a, 'b = ', b)
+
+let result = a +b;
+a = result - a;
+b = result - b;
 console.log('After swap a = ', a, 'b = ', b)

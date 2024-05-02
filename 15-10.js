cdf3264d84de472dab6d0ea678cb6a088e074933
Golam Azam <bcdazam@gmail.com>
2024-05-02 22:46:27 +0600

    practice

--- 15-10.js
+++ 15-10.js
@@ -6,7 +6,7 @@
     15-4 Find max of two values, find max of three values
     15-5 Find the largest element of an array
     15-6 Sum of all numbers in an array
-    6 Remove duplicate item from an array
-    7 Count the number of words in a string
-    8 Reverse a string
+    15-7 Remove duplicate item from an array
+    15-8 Count the number of words in a string
+    15-9 Reverse a string
 */

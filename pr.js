584a57123de173af0b3e29ebda5af9b9443a20c4
Golam Azam <bcdazam@gmail.com>
2024-05-08 11:03:45 +0600

    practice

--- pr.js
+++ pr.js
@@ -1,10 +1,14 @@
 let x = 10;
 let y = 20;
 
-console.log('Before swap a =', x, ';', 'b =', y)
+// console.log('Before swap a =', x, ';', 'b =', y)
 
-let temp = x + y;
-x = y;
-y = temp;
+// let temp = x + y;
+// x = y;
+// y = temp;
+// console.log('After swap a =', x, ';', 'b =', y)
 
+x = x + y;
+y = x - y;
+x = x - y;
 console.log('After swap a =', x, ';', 'b =', y)

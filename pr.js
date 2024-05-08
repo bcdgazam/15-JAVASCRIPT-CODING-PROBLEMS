93c0574dca21c9481cdef2d6889956c511d0d30c
Golam Azam <bcdazam@gmail.com>
2024-05-08 11:56:58 +0600

    random number generate

--- pr.js
+++ pr.js
@@ -8,7 +8,20 @@ let y = 20;
 // y = temp;
 // console.log('After swap a =', x, ';', 'b =', y)
 
-x = x + y;
-y = x - y;
-x = x - y;
-console.log('After swap a =', x, ';', 'b =', y)
+// x = x + y;
+// y = x - y;
+// x = x - y;
+// console.log('After swap a =', x, ';', 'b =', y)
+
+// [x, y ] = [y, x]
+// console.log('After swap a =', x, ';', 'b =', y)
+
+
+let num = 10.50;
+console.log(num);
+
+let result = Math.random(num) * 10;
+let fixedNumber = result.toFixed(0)
+let fixedResult = Math.abs(fixedNumber)
+
+console.log(fixedResult)

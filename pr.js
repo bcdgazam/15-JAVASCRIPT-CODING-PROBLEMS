e981cfbdc346b69b95a6d610a266cec1b1169feb
Golam Azam <bcdazam@gmail.com>
2024-05-09 17:57:31 +0600

    larget number from an array

--- pr.js
+++ pr.js
@@ -1,5 +1,5 @@
-let x = 10;
-let y = 20;
+// let x = 10;
+// let y = 20;
 
 // console.log('Before swap a =', x, ';', 'b =', y)
 
@@ -28,9 +28,9 @@ let y = 20;
 
 //Find max of two values
 
-let numOne = 1000;
-let numTwo = 300;
-let numThree = 4000;
+// let numOne = 1000;
+// let numTwo = 300;
+// let numThree = 4000;
 
 // if(numOne > numOne){
 //     console.log('Number One is bigger')
@@ -39,19 +39,50 @@ let numThree = 4000;
 //     console.log('Number Tow is bigger')
 // }
 
-if(numOne > numTwo){
-    if(numOne > numThree){
-        console.log('Number One is bigger')
-    }
-    else{
-        console.log('Number Three is bigger')
+// if(numOne > numTwo){
+//     if(numOne > numThree){
+//         console.log('Number One is bigger')
+//     }
+//     else{
+//         console.log('Number Three is bigger')
+//     }
+// }
+// else{
+//     if(numTwo > numThree){
+//         console.log('Number Two is bigger')
+//     }
+//     else{
+//         console.log('Number Three is bigger')
+//     }
+// }
+
+
+//Find the largest element of an array
+
+// let arr = [23, 45, 32, 22, 54, 66, 65, 90, 23, 5, 43, 531, 555,66666]
+
+// let max = arr[0];
+
+// for(let i = 0; i < arr.length; i++){
+//     let element = arr[i];
+//     if(element > max){
+//         max = element;
+//     }
+// }
+
+// console.log(max);
+
+//get max number from an array
+
+let stResult = [23, 45, 23, 53, 53, 98, 97, 90, 24, 45, 34]
+
+let max = stResult[0];
+
+for(let i = 0; i < stResult.length; i++){
+    let element = stResult[i];
+    if(element > max){
+        max = element;
     }
 }
-else{
-    if(numTwo > numThree){
-        console.log('Number Two is bigger')
-    }
-    else{
-        console.log('Number Three is bigger')
-    }
-}
+
+console.log(max);
